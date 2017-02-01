d1aa57936ab9a7699b4e249e2e1cd7aabe9379ca
rishabhrao <admin@rishabhrao.net>
2017-02-01 06:53:22 +0000

    AndroModX
    Added AndroModX defconfig
    
    Added Build Script for Quicker Compilation
    
    Updated Read ME
    
    Signed-off-by: rishabhrao <admin@rishabhrao.net>

---
+++ README.md
@@ -0,0 +1,72 @@
+LeEco Le 2 (s2) AndroModX Kernel     
+===============================
+
+
+The LeEco Le 2 is a smartphone from LeEco or LeMobile Information Technology Co. Ltd.
+
+
+
+Device configuration for s2
+=====================================
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+CHIPSET | Qualcomm MSM8976 Snapdragon 652
+CPU     | Quad-core 1.4 GHz Cortex-A53 & Quad-core 1.8 GHz Cortex-A72
+GPU     | Adreno 510
+Memory/RAM  | 3 GB
+Storage | 32 GB
+Battery | 3000 mAh (Non-Removable)
+Dimensions | 151.1 x 74.2 x 7.5 mm
+Display | 1080 x 1920 pixels 5.5"
+Rear Camera  | 16.0 MP
+Front Camera | 8.0 MP
+Release Date | June 2016
+
+
+
+![LeEco Le 2](http://in.img3.lemall.com/file/20160606/default/3370481864506311 "LeEco Le 2")
+
+
+
+# Build Guide:
+--------
+
+# Download, Prepare and Compile the Kernel
+--------
+
+
+- Download Toolchains (CAF Toolchains (Recommended) Used Below. Other Toolchains May Cause Problems.)
+
+
+		$ git clone https://source.codeaurora.org/quic/la/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b LA.BR.1.3.5.c1_rb1.3
+
+
+
+- Download Kernel Sources
+
+
+		$ git clone https://github.com/rishabhrao/AndroModX-s2.git -b master
+
+
+
+- Change Directory to AndroModX Kernel Directory and Make Output Folder
+
+
+		$ cd AndroModX-s2
+
+
+
+- Compile the Kernel
+
+
+		$ ./build.sh
+
+
+
+
+# 2. Get Compiled Kernel Output
+---------------
+
+
+- Kernel: arch/arm64/boot/Image.gz-dtb
---
+++ build.sh
@@ -0,0 +1,72 @@
+#
+# Copyright © 2016, Rishabh Rao "rishabhrao" <admin@rishabhrao.net>
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# Please maintain this if you use this script or any part of it
+
+# Init Script
+: ${KERNEL_DIR:=$PWD}
+export PATH=$PATH:$KERNEL_DIR
+export PATH=$PATH:$KERNEL_DIR/../aarch64-linux-android-4.9
+export PATH=$PATH:$KERNEL_DIR/../aarch64-linux-android-4.9/bin
+PATH=$PATH$( find $KERNEL_DIR/../aarch64-linux-android-4.9 -type d -printf ":%p" )
+IMAGE=$KERNEL_DIR/arch/arm64/boot/Image.gz-dtb
+DEFCONFIG=andromodx_defconfig
+: ${JOBS:=32}
+BUILD_START=$(date +"%s")
+
+# Color Code Script
+Black='\e[0;30m'        # Black
+Red='\e[0;31m'          # Red
+Green='\e[0;32m'        # Green
+Yellow='\e[0;33m'       # Yellow
+Blue='\e[0;34m'         # Blue
+Purple='\e[0;35m'       # Purple
+Cyan='\e[0;36m'         # Cyan
+White='\e[0;37m'        # White
+nocol='\033[0m'         # Default
+
+# Tweakable Options Below
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER="rishabhrao"
+export KBUILD_BUILD_HOST="AndroModX"
+export CROSS_COMPILE="aarch64-linux-android-"
+
+# Compilation Scripts Are Below
+compile_kernel ()
+{
+echo -e "$White***********************************************"
+echo "         Compiling AndroModX Kernel                     "
+echo -e "***********************************************$nocol"
+make clean && make mrproper;
+make $DEFCONFIG;
+make -j$JOBS;
+
+if ! [ -a $IMAGE ];
+then
+echo -e "$Red Kernel Compilation Failed! Fix the Errors! $nocol"
+exit 1
+fi
+}
+
+# Finalizing Script Below
+case $1 in
+clean)
+make ARCH=arm64 -j$JOBS clean mrproper
+;;
+*)
+compile_kernel
+;;
+esac
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "$Yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
